7decf63fc2c8d54d2e953329b9d0b2616be245c2
Evans Acheampong <evansachie01@gmail.com>
2024-10-12 11:26:57 +0000

    feat: add interfaces and mock data for Explore page

---
+++ frontend/src/data/topics.ts
@@ -0,0 +1,22 @@
+import ExploreTopicItem from '../interfaces/ExploreTopicItem';
+import TopicImg1 from '../assets/explore-1.svg';
+import TopicImg2 from '../assets/explore-2.svg';
+import TopicImg3 from '../assets/explore-3.svg';
+import TopicImg4 from '../assets/explore-4.svg';
+
+const mockTopics: ExploreTopicItem[] = [
+  { id: '1', title: 'Illegal Mining in Weija', imageUrl: TopicImg1 },
+  { id: '2', title: 'Water Pollution in Accra', imageUrl: TopicImg2 },
+  { id: '3', title: 'Land destruction in Nima', imageUrl: TopicImg3 },
+  { id: '4', title: 'Plastic Waste in Ghana', imageUrl: TopicImg4 },
+  { id: '5', title: 'Air Pollution in Tema', imageUrl: TopicImg1 },
+  { id: '6', title: 'Deforestation in Ashanti', imageUrl: TopicImg2 },
+  { id: '7', title: 'Illegal Mining in Weija', imageUrl: TopicImg1 },
+  { id: '8', title: 'Water Pollution in Accra', imageUrl: TopicImg2 },
+  { id: '9', title: 'Land destruction in Nima', imageUrl: TopicImg3 },
+  { id: '10', title: 'Plastic Waste in Ghana', imageUrl: TopicImg4 },
+  { id: '11', title: 'Air Pollution in Tema', imageUrl: TopicImg1 },
+  { id: '12', title: 'Deforestation in Ashanti', imageUrl: TopicImg2 },
+];
+
+export default mockTopics;
