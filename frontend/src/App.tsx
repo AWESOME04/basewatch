15cc6093b26f84e05dbb840b6a3cf0d2607667b5
Evans Acheampong <evansachie01@gmail.com>
2024-10-13 21:50:35 +0000

    feat: make corresponding updates to Header and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { WalletProvider } from './components/auth/WalletProvider';
 import HomePage from './pages/HomePage';
 import Feed from './pages/Feed';
 import Explore from './pages/Explore';
@@ -8,16 +9,18 @@ import SinglePostPage from './pages/SinglePostPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/maps" element={<ViewMap />} />
-        <Route path="/report" element={<ReportActivity />} />
-        <Route path="/post/:postId" element={<SinglePostPage />} />
-      </Routes>
-    </Router>
+    <WalletProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/maps" element={<ViewMap />} />
+          <Route path="/report" element={<ReportActivity />} />
+          <Route path="/post/:postId" element={<SinglePostPage />} />
+        </Routes>
+      </Router>
+    </WalletProvider>
   );
 }
 
