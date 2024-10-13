121dea49a3279f313c6c2afe76dc5129609ba99b
sedfarouk <sadikalhanssah@gmail.com>
2024-10-13 22:47:35 +0000

    add misc

--- Backend/BaseWatch.Api/obj/Debug/net8.0/BaseWatch.Api.AssemblyInfo.cs
+++ Backend/BaseWatch.Api/obj/Debug/net8.0/BaseWatch.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaseWatch.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d0562e9c607852079d282987de3308673e2c6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86accc2a5d8a8648c4c35791ac8dccc62f8a03a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaseWatch.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaseWatch.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
