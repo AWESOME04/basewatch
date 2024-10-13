7ba17c79bddd556dcf5e67f2d5f51c857fb26a4c
Evans Acheampong <evansachie01@gmail.com>
2024-10-13 05:48:07 +0000

    update: add link to explore topic section

--- frontend/src/components/explore/ExploreGrid.tsx
+++ frontend/src/components/explore/ExploreGrid.tsx
@@ -1,18 +1,21 @@
+import { Link } from 'react-router-dom';
 import mockTopics from '../../data/topics';
 
 
 const ExploreGrid = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {mockTopics.map((topic) => (
-        <div key={topic.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-          <img src={topic.imageUrl} alt={topic.title} className="w-full h-48 object-cover" />
-          <div className="p-4">
-            <h3 className="text-lg font-semibold">{topic.title}</h3>
+    <Link to='/feed'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {mockTopics.map((topic) => (
+          <div key={topic.id} className="bg-white rounded-lg shadow-md overflow-hidden">
+            <img src={topic.imageUrl} alt={topic.title} className="w-full h-48 object-cover" />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold">{topic.title}</h3>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Link>
   );
 };
 
